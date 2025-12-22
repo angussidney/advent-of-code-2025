8a126bc5b7c251c83ab5c34d4bfbc775d32db057
angussidney <4848802+angussidney@users.noreply.github.com>
2025-12-22 13:08:27 +1100

    Finish day1a

--- src/day1a.rs
+++ src/day1a.rs
@@ -1,8 +1,56 @@
 use std::str::Lines;
 
-pub fn run(_input: Lines) -> usize {
-    println!("Day1a!");
-    3
+enum Rotation {
+    Left(usize),
+    Right(usize),
+}
+
+struct Model {
+    start_rot: isize,
+    rotations: Vec<Rotation>,
+}
+
+fn parse_input(input: Lines) -> Model {
+    let mut rotations = Vec::new();
+    for line in input {
+        let mut chars = line.chars();
+        let dir = chars.next().unwrap();
+        let clicks = chars
+            .as_str()
+            .parse::<usize>()
+            .expect("Rotation should be an integer");
+        rotations.push(match dir {
+            'L' => Rotation::Left(clicks),
+            'R' => Rotation::Right(clicks),
+            _ => panic!("Invalid direction specified"),
+        })
+    }
+    Model {
+        start_rot: 50,
+        rotations: rotations,
+    }
+}
+
+fn calculate_result(model: Model) -> usize {
+    let mut pos = model.start_rot;
+    let mut zeroes = 0;
+    for rot in model.rotations {
+        pos = match rot {
+            Rotation::Left(clicks) => pos.checked_sub_unsigned(clicks).unwrap(),
+            Rotation::Right(clicks) => pos.checked_add_unsigned(clicks).unwrap(),
+        };
+        pos = pos.rem_euclid(100);
+
+        if pos == 0 {
+            zeroes += 1;
+        }
+    }
+    zeroes
+}
+
+pub fn run(input: Lines) -> usize {
+    let model = parse_input(input);
+    return calculate_result(model);
 }
 
 #[cfg(test)]
@@ -19,6 +67,6 @@ mod tests {
     #[ignore]
     fn challenge() {
         let data = include_str!("../tests/day1a/challenge.txt").lines();
-        assert_eq!(run(data), 0);
+        assert_eq!(run(data), 1147);
     }
 }
